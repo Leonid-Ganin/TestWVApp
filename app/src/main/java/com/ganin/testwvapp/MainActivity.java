1c71392027d7172be128170ef35eaeb94b151c55
Leonid-Ganin <Ganin-Leo.nid@yandex.ru>
2023-02-02 18:09:45 +0300

    Implement network access screen

--- app/src/main/java/com/ganin/testwvapp/MainActivity.java
+++ app/src/main/java/com/ganin/testwvapp/MainActivity.java
@@ -1,16 +1,22 @@
 package com.ganin.testwvapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
@@ -23,8 +29,93 @@ public class MainActivity extends AppCompatActivity {
     public static final String URL = "url";
 
     private WebView webView;
-    private String url = "";
+    private TextView textView;
     private SharedPreferences mSettings;
+    private String url = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        textView = findViewById(R.id.textView);
+
+        mSettings = getSharedPreferences(PREF, MODE_PRIVATE);
+        url = mSettings.getString(URL, "");
+
+        if (url.equals("")) {
+            FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+            FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                    .setMinimumFetchIntervalInSeconds(3600)
+                    .build();
+            mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+            mFirebaseRemoteConfig.fetchAndActivate()
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            url = mFirebaseRemoteConfig.getString(URL);
+
+                            if (url.isEmpty() || checkIsEmu()) {
+                                // Добавить заглушку
+                                return;
+                            }
+
+                            mSettings.edit().putString(URL, url).apply();
+
+                            if (hasConnection(this)) {
+                                initWebview(savedInstanceState);
+                            } else {
+                                webView.setVisibility(View.GONE);
+                            }
+                        } else {
+                            webView.setVisibility(View.GONE);
+                        }
+                    });
+        } else {
+            if (hasConnection(this)) {
+                initWebview(savedInstanceState);
+            } else {
+                webView.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+
+    private void initWebview(Bundle savedInstanceState) {
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setSupportZoom(false);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else
+            webView.loadUrl(url);
+    }
 
     private boolean checkIsEmu() {
         if (BuildConfig.DEBUG) return false;
@@ -54,72 +145,28 @@ public class MainActivity extends AppCompatActivity {
                 || (brand.startsWith("generic") && Build.DEVICE.startsWith("generic"));
     }
 
-    private void createWebview(Bundle savedInstanceState) {
-        webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
+    private boolean hasConnection(Context context) {
+        try {
+            ConnectivityManager cm = (ConnectivityManager)
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setSupportZoom(false);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAllowContentAccess(true);
+            if (wifiInfo != null && wifiInfo.isConnected()) {
+                return true;
+            }
 
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
+            wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 
-        if (savedInstanceState != null)
-            webView.restoreState(savedInstanceState);
-        else
-            webView.loadUrl(url);
-    }
+            if (wifiInfo != null && wifiInfo.isConnected()) {
+                return true;
+            }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            wifiInfo = cm.getActiveNetworkInfo();
 
-        mSettings = getSharedPreferences(PREF, MODE_PRIVATE);
-        url = mSettings.getString(URL, "");
-
-        if (url.equals("")) {
-            FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
-            FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
-                    .setMinimumFetchIntervalInSeconds(3600)
-                    .build();
-            mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
-
-            mFirebaseRemoteConfig.fetchAndActivate()
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            url = mFirebaseRemoteConfig.getString(URL);
-
-                            if (url.isEmpty() || checkIsEmu()) {
-                                // Добавить заглушку
-                                return;
-                            }
-
-                            mSettings.edit().putString(URL, url).apply();
-                            createWebview(savedInstanceState);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Fetch failed",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        } else {
-            // Добавить проверку включён ли интернет
-            createWebview(savedInstanceState);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
+            return wifiInfo != null && wifiInfo.isConnected();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
