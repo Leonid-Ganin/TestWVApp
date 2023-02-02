e9f4e8352fa0011c4f652e5ac2cfad61923ae76e
Leonid-Ganin <Ganin-Leo.nid@yandex.ru>
2023-02-02 15:17:23 +0300

    Implement getting a link

--- app/src/main/java/com/ganin/testwvapp/MainActivity.java
+++ app/src/main/java/com/ganin/testwvapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.ganin.testwvapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+        mFirebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        String url = mFirebaseRemoteConfig.getString("url");
+
+                        if (url.isEmpty()) {
+                            return;
+                        }
+
+                        Toast.makeText(MainActivity.this, mFirebaseRemoteConfig.getString("url"),
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Fetch failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
